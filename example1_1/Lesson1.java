d9c6680bb324bee91aa7725469489ebf6f615d42
glae83 <glae83@gmail.com>
2022-04-19 21:09:39 +0400

    19-04-2022 night

--- example1_1/Lesson1.java
+++ example1_1/Lesson1.java
@@ -1,4 +1,15 @@
 package example1_1;
 
 public class Lesson1 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        String te = "Ivan";
+     /*   System.out.println(a);*/
+        text(te);
+    }
+
+    public static void text (String te) {
+        System.out.println("Write text "+ te);
+    }
 }
