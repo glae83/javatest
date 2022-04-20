48e7f8e6dc431ac059c91f6dde24f5174267778b
glae83 <glae83@gmail.com>
2022-04-20 16:53:02 +0400

    20-04-2022

--- example1_1/Lesson1.java
+++ example1_1/Lesson1.java
@@ -4,12 +4,13 @@ public class Lesson1 {
 
     public static void main(String[] args) {
         int a = 10;
-        String te = "Ivan";
-     /*   System.out.println(a);*/
+
+        int te %=a;
+        /*   System.out.println(a);*/
         text(te);
     }
 
-    public static void text (String te) {
-        System.out.println("Write text "+ te);
+    public static void text(String te) {
+        System.out.println("Write text " + te);
     }
 }
